2499d99084202377e6debf9772ed25331b90b986
Selena de Leon <50776370+selenajdeleon@users.noreply.github.com>
2021-05-05 02:04:13 -0700

    pushing Slides link to repo

--- Selena-dL/Queries/join_query.sql
+++ Selena-dL/Queries/join_query.sql
@@ -1,7 +1,7 @@
--- test.sql For creating tables for clean/merged data
+-- Query creating tables for clean/merged data
 
 -- test table for all Date's
-CREATE TABLE test (
+CREATE TABLE skeleton (
 	"Date" DATE NOT NULL,
 	PRIMARY KEY ("Date")
 );
@@ -16,7 +16,6 @@ CREATE TABLE CO (
 	--  FOREIGN KEY ("Date") REFERENCES test ("Date"),
 	 PRIMARY KEY ("Date", AQI_CO)
 );
-
 
 -- Create table for NO2 2015-2020
 CREATE TABLE NO2 (
@@ -62,7 +61,7 @@ CREATE TABLE PM_2_5 (
      PRIMARY KEY ("Date", AQI_PM_2_5)
 );
 
--- View table
+-- View tables
 SELECT * FROM co;
 SELECT * FROM no2;
 SELECT * FROM o3;
@@ -71,93 +70,32 @@ SELECT * FROM pm_10;
 
 -----------------------------------------------
 
--- Joining tables w/Date,AQI_[pollutant]
+-- JOINing tables on Date & AQI_[pollutant]
 
--- SELECT test."Date",
--- 	co.Daily_Max_8_hr_CO_Concentration,
--- 	co.UNITS,
--- 	co.AQI_CO,
--- 	co.Site_Name
--- FROM test
--- LEFT JOIN co
--- ON test."Date" = co."Date";
-
-SELECT * FROM test;
-
--- -- pm_10
--- SELECT test."Date",
--- 	pm_10.AQI_PM_10
--- INTO join_1
--- FROM test
--- LEFT JOIN pm_10
--- ON test."Date" = pm_10."Date";
-
--- SELECT * FROM join_1;
-
--- -- co
--- SELECT test."Date",
--- 	co.AQI_CO,
--- 	join_1.AQI_PM_10
--- -- INTO join_2
--- FROM test
--- LEFT JOIN join_1
--- ON join_1."Date" = test."Date"
--- INNER JOIN co
--- ON join_1."Date" = co."Date"
--- ORDER BY co."Date"
--- ;
-
-
--- UPDATE table SET col1 = 0 WHERE col1 IS NULL;
--- UPDATE join_1 SET AQI_PM_10 = NULL WHERE AQI_PM_10 IS "0";
-
-
--- -- no2
--- SELECT test."Date",
--- 	no2.AQI_NO2
--- -- INTO join_3
--- FROM test
--- LEFT JOIN no2
--- ON test."Date" = no2."Date";
-
--- SELECT * FROM join_3;
+-- View skeleton table
+SELECT * FROM skeleton;
 
 -- Joining AQI across pollutants
-SELECT test."Date",
+SELECT skeleton."Date",
 	co.AQI_CO,
 	no2.AQI_NO2,
 	o3.AQI_O3,
 	pm_2_5.AQI_PM_2_5,
 	pm_10.AQI_PM_10
 INTO aqi_all
-FROM test
+FROM skeleton
 LEFT JOIN co
-ON co."Date" = test."Date"
+ON co."Date" = skeleton."Date"
 LEFT JOIN no2
-ON no2."Date" = test."Date"
+ON no2."Date" = skeleton."Date"
 LEFT JOIN o3
-ON o3."Date" = test."Date"
+ON o3."Date" = skeleton."Date"
 LEFT JOIN pm_2_5
-ON pm_2_5."Date" = test."Date"
+ON pm_2_5."Date" = skeleton."Date"
 LEFT JOIN pm_10
-ON pm_10."Date" = test."Date"
-ORDER BY test."Date"
+ON pm_10."Date" = skeleton."Date"
+ORDER BY skeleton."Date"
 ;
 
+-- View aqi_all
 SELECT * FROM aqi_all;
-
-
--- -- Filtering for Max AQI value in row
--- SELECT a_a."Date", 
--- a_a.AQI_CO, 
--- a_a.AQI_NO2, 
--- a_a.AQI_O3, 
--- a_a.AQI_PM_2_5, 
--- a_a.AQI_PM_10
--- FROM aqi_all as a_a
--- WHERE (AQI_CO = (SELECT MAX(AQI_CO) FROM aqi_all))
--- AND (AQI_NO2 = (SELECT MAX(AQI_NO2) FROM aqi_all))
--- AND (AQI_O3 = (SELECT MAX(AQI_O3) FROM aqi_all))
--- AND (AQI_PM_2_5 = (SELECT MAX(AQI_PM_2_5) FROM aqi_all))
--- AND (AQI_PM_10 = (SELECT MAX(AQI_PM_10) FROM aqi_all))
--- ;
